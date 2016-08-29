52670b63b9b59a6774dd1ea4bff91acef77a4553
evas14 <evas14@ru.is>
2016-08-29 18:16:47 +0000

    Setting up the game

---
+++ SnjollStaerdfraedi/Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    //Adds a reference to GameController
+    [SerializeField]
+    private InputField input;
+
+    //This function runs as soon as game begins.
+    //Here we initialize values.
+    void Awake()
+    {
+        //input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    public void GetInput(string guess)
+    {
+        Debug.Log("You entered " + guess);
+
+        //Resets input.
+        input.text = "";
+    }
+
+}
